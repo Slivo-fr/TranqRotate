48e6e55d3367f891d13734cf79a58b339419a8bd
Slivo <gchopelet@gmail.com>
2021-05-12 13:53:12 +0200

    Fix checkVersions lua error with non hunter players

--- src/tranqRotate.lua
+++ src/tranqRotate.lua
@@ -260,7 +260,7 @@ function TranqRotate:checkVersions()
     end
     for key, player in pairs(TranqRotate.addonVersions) do
         if (player ~= UnitName("player")) then
-            TranqRotate:printPrefixedMessage(player .. " - " .. TranqRotate:formatAddonVersion(hunter.addonVersion))
+            TranqRotate:printPrefixedMessage(player .. " - " .. TranqRotate:formatAddonVersion(player.addonVersion))
         end
     end
 end
