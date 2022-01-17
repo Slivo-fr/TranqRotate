0fe848460e0206901379389b7c7b22e7ff8324a6
Slivo <gchopelet@gmail.com>
2022-01-17 16:18:40 +0100

    Fix incapacitated backup alert

--- src/debuff.lua
+++ src/debuff.lua
@@ -1,19 +1,24 @@
 -- Checks if player is incapacitated by a debuff for too long
 function TranqRotate:isPlayedIncapacitatedByDebuff()
-
     for i, debuffId in ipairs(TranqRotate.constants.incapacitatingDebuffs) do
-
-        local name, _, _, _, _, expirationTime, _, _, _, spellId, _, isBossDebuff
-            = AuraUtil.FindAuraByName(GetSpellInfo(debuffId), "player")
-
-        if (name) then
-            print(name, expirationTime - GetTime(), spellId, isBossDebuff)
-
-            if (expirationTime - GetTime() > TranqRotate.constants.incapacitatedDelay) then
-                return true
-            end
+        local name, expirationTime = TranqRotate:getPlayerDebuff(debuffId)
+        if (name and expirationTime - GetTime() > TranqRotate.db.profile.incapacitatedDelay) then
+            return true
         end
     end
 
     return false
+end
+
+function TranqRotate:getPlayerDebuff(debuffId)
+    for i=1, 32, 1 do
+        local name, icon, count, dispelType, duration, expirationTime, source, isStealable, nameplateShowPersonal,
+        spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod = UnitDebuff("player", i)
+
+        if (spellId and spellId == debuffId) then
+            return name, expirationTime
+        end
+    end
+
+    return nil
 end
